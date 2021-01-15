facdd56904865ab01d6b090f72a515dfcc541bb7
Patrick <patrick@godknows.de>
2021-01-15 09:30:11 +0100

    New Controller & GET a New Character

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,18 @@
+using dotnet_rpg.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        private static Character knight = new Character();
+
+        [HttpGet]
+        public ActionResult<Character> Get()
+        {
+            return Ok(knight);
+        }
+    }
+}
