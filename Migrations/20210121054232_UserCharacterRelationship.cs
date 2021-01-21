61c1f8d5cae070ce5ba204c3766e5d15133b87d6
Patrick <patrick@godknows.de>
2021-01-21 06:49:00 +0100

    First Relationship

---
+++ Migrations/20210121054232_UserCharacterRelationship.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace dotnet_rpg.Migrations
+{
+    public partial class UserCharacterRelationship : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Characters",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Characters_UserId",
+                table: "Characters",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Characters_Users_UserId",
+                table: "Characters",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Characters_Users_UserId",
+                table: "Characters");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Characters_UserId",
+                table: "Characters");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Characters");
+        }
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -8,5 +8,6 @@ namespace dotnet_rpg.Models
         public string Username { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public List<Character> Characters { get; set; }
     }
 }
