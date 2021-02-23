d99f435e9a70a1977341ae43ed569cd00a15fcf8
Patrick <patrick@godknows.de>
2021-02-23 06:44:50 +0100

    Authentication Controller

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+using System.Threading.Tasks;
+using dotnet_rpg.Data;
+using dotnet_rpg.Dtos.User;
+using dotnet_rpg.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _authRepo;
+        public AuthController(IAuthRepository authRepo)
+        {
+            _authRepo = authRepo;
+
+        }
+
+        [HttpPost("Register")]
+        public async Task<ActionResult<ServiceResponse<int>>> Register(UserRegisterDto request)
+        {
+            var response = await _authRepo.Register(
+                new User { Username = request.Username }, request.Password
+            );
+
+            if(!response.Success)
+            {
+                return BadRequest(response);
+            }
+
+            return Ok(response);
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -39,6 +39,7 @@ namespace dotnet_rpg
             });
             services.AddAutoMapper(typeof(Startup));
             services.AddScoped<ICharacterService, CharacterService>();
+            services.AddScoped<IAuthRepository, AuthRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
