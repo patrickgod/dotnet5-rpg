55eae08cf9255d22167c998ccfa9909c54d32de3
Patrick <patrick@godknows.de>
2021-03-24 16:35:24 +0100

    One-To-One Relationship with Weapons

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -13,5 +13,7 @@ namespace dotnet_rpg.Data
         public DbSet<Character> Characters { get; set; }
 
         public DbSet<User> Users { get; set; }
+
+        public DbSet<Weapon> Weapons { get; set; }
     }
 }
