f1de3cd4447a05a262196225250ea12883dbf9df
Patrick <patrick@godknows.de>
2021-03-31 10:56:40 +0200

    Attack with Skills

--- Services/FightService/FightService.cs
+++ Services/FightService/FightService.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using dotnet_rpg.Data;
 using dotnet_rpg.Dtos.Fight;
@@ -28,6 +29,54 @@ namespace dotnet_rpg.Services.FightService
                     .FirstOrDefaultAsync(c => c.Id == request.OpponentId);
 
                 int damage = attacker.Weapon.Damage + (new Random().Next(attacker.Strength));
+                damage -= new Random().Next(opponent.Defense);
+
+                if(damage > 0)
+                    opponent.HitPoints -= damage;
+
+                if(opponent.HitPoints <= 0)
+                    response.Message = $"{opponent.Name} has been defeated!";
+
+                await _context.SaveChangesAsync();
+
+                response.Data = new AttackResultDto
+                {
+                    Attacker = attacker.Name,
+                    AttackerHP = attacker.HitPoints,
+                    Opponent = opponent.Name,
+                    OpponentHP = opponent.HitPoints,
+                    Damage = damage
+                };
+            }
+            catch (Exception ex)
+            {
+                response.Success = false;
+                response.Message = ex.Message;
+            }
+            return response;
+        }
+
+        public async Task<ServiceResponse<AttackResultDto>> SkillAttack(SkillAttackDto request)
+        {
+            var response = new ServiceResponse<AttackResultDto>();
+            try
+            {
+                var attacker = await _context.Characters
+                    .Include(c => c.Skills)
+                    .FirstOrDefaultAsync(c => c.Id == request.AttackerId);
+
+                var opponent = await _context.Characters
+                    .FirstOrDefaultAsync(c => c.Id == request.OpponentId);
+
+                var skill = attacker.Skills.FirstOrDefault(s => s.Id == request.SkillId);
+                if(skill == null)
+                {
+                    response.Success = false;
+                    response.Message = $"{attacker.Name} doesn't know this skill.";
+                    return response;
+                }
+
+                int damage = skill.Damage + (new Random().Next(attacker.Intelligence));
                 damage -= new Random().Next(opponent.Defense);
 
                 if(damage > 0)
--- Services/FightService/IFightService.cs
+++ Services/FightService/IFightService.cs
@@ -7,5 +7,6 @@ namespace dotnet_rpg.Services.FightService
     public interface IFightService
     {
         Task<ServiceResponse<AttackResultDto>> WeaponAttack(WeaponAttackDto request);
+        Task<ServiceResponse<AttackResultDto>> SkillAttack(SkillAttackDto request);
     }
 }
